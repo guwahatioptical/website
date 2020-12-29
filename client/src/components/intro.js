efe5a5df2432d62680b3f4ec6891a69eaf8eeb06
Amit Dey <apu76899@gmail.com>
2020-12-30 02:26:11 +0530

    'dev-phase'

--- client/src/components/intro.js
+++ client/src/components/intro.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import "../css/intro.css"
+import specsFrame from "../images/logo.png" 
 
 function Intro() {
     return (
         <div className="intro">
             <div className="intro-body animate">
-
+               <img className="specsframe" alt="intro specsframe" src={specsFrame} />
             </div>
         </div>
     )
