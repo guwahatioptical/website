32a3354d04d6f00677270e10f7a6e1cd015e3f47
Amit Dey <apu76899@gmail.com>
2021-01-23 01:23:46 +0530

    dev-phase

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,18 @@
-
+import react,{useState, useEffect} from 'react'
 import Intro from './intro';
 import MainPage from './MainPage'
 import '../css/Main.css';
 
 function Main() {
+  const [splash, setSplash] = useState(true)
+  window.addEventListener('load',()=>{
+    setTimeout(()=>{
+      setSplash(false)
+    },3700)
+  })
   return (
     <div className="App">
-      <Intro />
+      {splash?<Intro />:null}
       <MainPage />
     </div>
   );
