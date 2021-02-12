cf524b9d20a213a1ee003a7c4fd4f8cc6ae479b9
Amit Dey <apu76899@gmail.com>
2021-02-12 11:07:19 +0530

    dev

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -34,47 +34,12 @@ function MainPage() {
     const [name, setName] = useState("")
     const [email, setEmail] = useState("")
     const [message, setMessage] = useState("")
-    const encode = (data) => {
-        return Object.keys(data)
-            .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-            .join("&");
-      }
-    const handleSubmit = e => {
-        e.preventDefault();
-        fetch("/", {
-          method: "POST",
-          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: encode({ "form-name": "contact", name, email, message })
-        })
-          .then(() => alert("Success!"))
-          .catch(error => alert(error));
-  
-        
-      }
+
           return (
         <div className="mainpage-body">
             <Navbar />
             <div className="cms">
-            <form onSubmit={handleSubmit}>
-          <p>
-            <label>
-              Your Name: <input type="text" name="name" value={name} onChange={e=>setName(e.target.value)} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your Email: <input type="email" name="email" value={email} onChange={e=>setEmail(e.target.value)} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Message: <textarea name="message" value={message} onChange={e=>setMessage(e.target.value)} />
-            </label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
+           
             </div>
         </div>
     )
