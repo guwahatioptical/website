d18e03fb87f0cff77efc9998a6ea5b861fbb3f4f
Amit Dey <apu76899@gmail.com>
2021-03-14 22:49:52 +0530

    dev

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -2,9 +2,13 @@ import React,{useState} from 'react'
 import "../css/mainpage.css"
 import logo from "../images/logo_.png"
 import { IoMenu } from "react-icons/io5";
+import { IoMdCall, IoMdMail } from "react-icons/io";
+import { FaFacebookF, FaRegAddressCard } from "react-icons/fa";
+import { SiInstagram } from "react-icons/si";
 import Iframe from 'react-iframe'
 import {BiMenuAltRight} from "react-icons/bi"
 import Carousel from "./Carousel";
+import Reviews from './Reviews'
 // import Chatbox from "./ChatBox";
 
 function Navbar(){
@@ -26,10 +30,10 @@ function Navbar(){
             <div><img onClick={scrollTop} src={logo} alt="brand logo" /></div>
             <div className="menu l">{menu?<BiMenuAltRight onClick={()=>setMenu(!menu)} />:<IoMenu onClick={()=>setMenu(!menu)} />}</div>
             <div className="links">
-                <div className="nav_link">About Us</div>
                 <div className="nav_link">Customer Reviews</div>
+                <div className="nav_link">Brands</div>
                 <div className="nav_link">Gallery</div>
-                <div className="nav_link">Contact Us</div>
+                <div className="nav_link">Opening Hours</div>
                 <div className="nav_link">Locate Us</div>
             </div>
             <div style={menu && window.innerWidth<551?{transform:'translateX(0vw)'}:{transform:'translateX(-100%)'}} className="mob_links">
@@ -45,7 +49,8 @@ function Navbar(){
 
 function Location() {
     return (
-      <div style={{ height: "80vh", width: "60vh" }}>
+      <div className="locate-container">
+        <h3 className="heading">Locate Us</h3>
         <Iframe
           src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBtpCoYQ-cOKg9jmgtEsq84PfbP8BftMLA
     &q=Commerce College, Commercial Point, 1st Floor, Kabita Mansion, Rajgarh Link Rd, near Guwahati, Chandmari, Guwahati, Assam"
@@ -56,13 +61,91 @@ function Location() {
     );
 }
 
-function MainPage({frame}) {
+function AboutUs() {
+  return (
+    <div className="aboutus-container">
+      <h3 className="heading">About Us</h3>
+    <div className="aboutus">
+      We are dedicated to providing you with the highest standards
+      of professional eye care, together with a friendly service, quality
+      products and excellent value. As an established independent opticians, we
+      are free to select the best products from any manufacturer. This means we
+      are able to offer you a wide choice of eyewear with quality frames. Our
+      commitment to quality extends to the people who look after your eyesight.
+      {/* At our practice your eyes will be examined by a qualified optometrist,
+      while your spectacle or contact lenses will be provided by fully trained
+      dispensing staff. You will always receive professional advice on your
+      vision and on choosing the best products for your needs, appearance and
+      lifestyle. */}
+    </div></div>
+  );
+}
+
+function ContactDetails() {
+  return (
+    <div className="footer">
+      <h3 className="footer-heading">Get in Touch</h3>
+      <div className="contact-details-body">
+        <div className="socialbar">
+          <div title="@guwahatioptical" className="icon">
+            <a
+              href="https://www.facebook.com/guwahatioptical/?ti=as"
+              target="blank"
+            >
+              <FaFacebookF />
+            </a>
+          </div>
+          <div title="@guwahatioptical" className="icon">
+            <a href="https://www.instagram.com/guwahatioptical/" target="blank">
+              <SiInstagram />
+            </a>
+          </div>
+        </div>
+        <div className="details">
+          <div className="address">
+            <h4>
+              <FaRegAddressCard /> Visit Us
+            </h4>
+            <p>
+              Commerce College, Commercial Point, 1st Floor, Kabita Mansion,
+              Rajgarh Link Rd, near Guwahati, Chandmari, Guwahati, Assam 781003
+            </p>
+          </div>
+          <div className="mail">
+            <h4>
+              <IoMdMail /> Mail Us
+            </h4>
+            <a href="mailto:guwahatioptical0@gmail.com">
+              guwahatioptical0@gmail.com
+            </a>
+          </div>
+          <div className="call">
+            <h4>
+              <IoMdCall /> Call Us
+            </h4>
+            <a href="tel:75760 98413">75760 98413</a>
+          </div>
+        </div>
+        <div className="copyright">
+          <img src={logo} alt="logo" />
+          <div>@ All Rights Reserved</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function MainPage() {
 
           return (
             <div className="mainpage-body">
               <Navbar />
-                <Carousel frame={frame}  className="start"/>
-                <Location />
+              <Carousel className="start" />
+              <Reviews />
+              <AboutUs />
+              <Location />
+              <div className="endname"></div>
+              <ContactDetails />
                 {/* <Chatbox /> */}
             </div>
           );
---
+++ src/components/Reviews.js
@@ -0,0 +1,140 @@
+import React from 'react'
+import '../css/reviews.css'
+import { FaRegStar, FaStar, FaStarHalfAlt } from "react-icons/fa";
+
+function Review({ r_index, fullName, review, rating, color }) {
+    const rates = rate =>{
+      switch (rate) {
+        case 1:
+          return [1, 0, 0, 0, 0];
+        case 1.5:
+          return [1, 0.5, 0, 0, 0];
+        case 2:
+          return [1, 1, 0, 0, 0];
+        case 2.5:
+          return [1, 1, 0.5, 0, 0];
+        case 3:
+          return [1, 1, 1, 0, 0];
+        case 3.5:
+          return [1, 1, 1, 0.5, 0];
+        case 4:
+          return [1, 1, 1, 1, 0];
+        case 4.5:
+          return [1, 1, 1, 1, 0.5];
+        case 5:
+          return [1, 1, 1, 1, 1];
+        default:
+          return 0;
+      }
+  }
+
+    return (
+        <div
+          style={{
+            backgroundColor: color,
+            float: r_index % 2 === 0 ? "left" : "right",
+          }}
+          className="review-container"
+        >
+          <div className="name">
+            {fullName} |{" "}
+            {rates(rating).map((e, index) => {
+              if (e <= 0) return <FaRegStar key={index} />;
+              else if (e < 1) return <FaStarHalfAlt key={index} />;
+              else return <FaStar key={index} />;
+            })}
+          </div>
+          <div className="review">{review}</div>
+        </div>
+    );
+}
+
+function Reviews() {
+    const reviews = [
+      {
+        fullName: `MRIJESH M.`,
+        review: `Spectacle buying experience has never been so easy before. I loved their pricing and customer service. Will definitely shop here again!`,
+        rating: 4,
+        color: "rgb(139, 218, 255)",
+      },
+      {
+        fullName: `SMRUTHI B.`,
+        review: `Top-notch frames at very reasonable prices.`,
+        rating: 5,
+        color: "rgb(255 177 138)",
+      },
+      {
+        fullName: `MUDITH G.`,
+        review: `My friend had recommended me to Guwahati Optical & I totally agree with her statement - Stylish products & budget friendly!`,
+        rating: 4,
+        color: "rgb(172 255 139)",
+      },
+      {
+        fullName: `SHRADDHA R.`,
+        review: `Great price, great quality, great styles, great customer service. First-grade products with affordable prices. I’ve already recommended Guwahati Optical to three friends.`,
+        rating: 4,
+        color: "rgb(255 139 139)",
+      },
+      {
+        fullName: `SOUMYA A.`,
+        review: `The spectacles are of high quality and economical!`,
+        rating: 3.5,
+        color: "rgb(255 247 139)",
+      },
+      {
+        fullName: `RADHA H.`,
+        review: `Tried it for the first time and totally in love with the experience. Economical, wide range of options & impeccable QUALITY!`,
+        rating: 3.5,
+        color: "rgb(190 139 255)",
+      },
+      {
+        fullName: `ANDREA S.`,
+        review: `This is my third pair of spectacles from here and can definitely regard Guwahati Optical as the epitome of quality & SERVICE.`,
+        rating: 4,
+        color: "#00bcd4",
+      },
+      {
+        fullName: `OM PRAKASH R.`,
+        review: `Recently got a pair of glasses with blue cut lenses. They are amazing at affordable price.`,
+        rating: 3.5,
+        color: "#8bc34a",
+      },
+      {
+        fullName: `KRISHNA J.`,
+        review: `Well,my experience with the store is highly satisfactory...They offer eye fashion which one can't refuse ...I recommend the store to all age groups especially women and kids...`,
+        rating: 4.5,
+        color: "rgb(255 210 139)",
+      },
+      {
+        fullName: `JONES K.`,
+        review: `Excellent customer service! Staff was amiable & helpful.`,
+        rating: 4,
+        color: "#ffc107",
+      },
+  ];
+  const getReviews = () => {
+    let start = 0, end = 0, len = reviews.length, n = Math.floor(Math.random() * len)
+    if (len - n > 6) {
+      start = n
+      end = n + 6    }
+    else if (len - n <= 3) {
+      start = n - 6;
+      end = n;
+    } else{
+      start = n - 3;
+      end = n+3;
+    }
+    return reviews.slice(start,end)
+  }
+    return (
+      <div>
+        <h3 className="review-heading">Happy Customers</h3>
+        <div className="reviews">  {getReviews().map((review, index) => 
+          <Review key={index} r_index={index} {...review} />
+        )
+        }</div>
+        </div>
+    )
+}
+
+export default Reviews
